50518b9350d42a04d91106ed5a17846e344b1789
Nicholas DeMarco <50844654+Ndemco@users.noreply.github.com>
2021-03-26 16:52:45 -0400

    logout user endpoint

--- back-end/src/middleware/auth.js
+++ back-end/src/middleware/auth.js
@@ -3,22 +3,23 @@ const User = require('../models/user')
 
 const auth = async (req, res, next) => {
    try {
-      const token = req.cookies.Authorization.replace('Bearer ', '')
+      const token = req.cookies.Authorization.replace('Bearer ', '');
 
-      const decoded = jwt.verify(token, 'thisisasecret')
+      const decoded = jwt.verify(token, 'thisisasecret');
 
       // find user with correct ID who has proper authentication token
-      const user = await User.findOne({ _id: decoded._id, 'tokens.token': token })
+      const user = await User.findOne({ _id: decoded._id, 'tokens.token': token });
 
       // user not found
       if (!user) {
-         throw new Error()
+         throw new Error();
       }
 
-      req.user = user
-      next()
-   } catch (e) {
-      res.status(401).send({ error: 'Please authenticate.' })
+      req.token = token;
+      req.user = user;
+      next();
+   } catch (err) {
+      res.status(401).send({ error: 'Please authenticate.' });
    }
 }
 
