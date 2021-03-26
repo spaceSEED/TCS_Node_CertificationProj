d8f1021939b803a7e327087ccdc7d4b302900b70
jiachengli1999 <35944405+jiachengli1999@users.noreply.github.com>
2021-03-26 11:18:46 -0400

    added components

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { WeatherComponent } from './components/weather/weather.component';
+import { NewsComponent } from './components/news/news.component';
+import { GallaryComponent } from './components/gallary/gallary.component';
+import { ChatboxComponent } from './components/chatbox/chatbox.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    WeatherComponent,
+    NewsComponent,
+    GallaryComponent,
+    ChatboxComponent
   ],
   imports: [
     BrowserModule,
