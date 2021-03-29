a1abb3a28b91e12646a78727d01b647422b6745a
Liam Brown <spaceSEED@users.noreply.github.com>
2021-03-29 11:09:57 -0400

    header change

--- back-end/src/routes/index.js
+++ back-end/src/routes/index.js
@@ -4,19 +4,19 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { page:0 });
+  res.render('index', { page:0, token:req.headers.cookie });
 });
 
 router.get('/newsform', function(req, res, next) {
-  res.render('newsform', { page:1 });
+  res.render('newsform', { page:1, token:req.headers.cookie });
 });
 
 router.get('/edit-news', function(req, res, next) {
-  res.render('edit-news', { page:2 });
+  res.render('edit-news', { page:2, token:req.headers.cookie });
 });
 
 router.get('/sportform', function(req, res, next) {
-  res.render('sportform', { page:3 });
+  res.render('sportform', { page:3, token:req.headers.cookie });
 });
 
 router.get('/chat', function(req, res, next) {
--- back-end/src/routes/users.js
+++ back-end/src/routes/users.js
@@ -10,7 +10,7 @@ router.post('/', async (req, res) => {
   try {
     await user.save();
     const token = await user.generateAuthToken();
-    return res.status(201).send({ user, token });
+    res.status(201).cookie('Authorization', token).redirect('/');//.send({user,token});
   } catch (err) {
     return res.status(400).send(err);
   }
@@ -18,12 +18,13 @@ router.post('/', async (req, res) => {
 
 // GET login //
 router.get('/login', async (req, res) => {
-  res.render('login', { page: 5 });
+  console.log(req.headers.cookie);
+  res.render('login', { page: 5, token:req.headers.cookie });
 });
 
 // GET signup //
 router.get('/signup', async (req, res) => {
-  res.render('signup', { page: 6 });
+  res.render('signup', { page: 6, token:req.headers.cookie });
 });
 
 /* Login user */
@@ -31,7 +32,7 @@ router.post('/login', async (req, res) => {
   try {
     const user = await User.findByCredentials(req.body.email, req.body.password);
     const token = await user.generateAuthToken();
-    res.status(200).cookie('Authorization', token).send({ user, token });
+    res.status(200).cookie('Authorization', token).redirect('/');
   } catch (err) {
     res.status(500).send(err);
   }
@@ -46,14 +47,14 @@ router.post('/logout', auth, async (req, res) => {
 
           //walk array of tokens
           //if token in array not equal to token logging out, keep it
-          return token.token !== req.token
+          return token.token !== req.token;
       })
 
-      await req.user.save()
+      await req.user.save();
 
-      res.status(200).send()
+      res.status(200).redirect('/');
   } catch (err) {
-      res.status(400).send(err)
+      res.status(400).send(err);
   }
 })
 
