bc4b68156091f398caf07c33118aa46d1c175b86
ammaressajee <ammar.essajee97@gmail.com>
2021-03-30 15:06:27 -0500

    email verification

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -10,13 +10,15 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var newsRouter = require('./routes/news');
 var sportsRouter = require('./routes/sports');
-require('./db/mongoose')
+require('./db/mongoose');
 
+// body parser and nodemailer for email implementation
+var nodemailer = require('nodemailer');
 
 var app = express();
 
 //CORS ["http://localhost:3000","http://localhost:4200"]
-app.use(cors({origin:true}));
+app.use(cors({ origin: true }));
 /*app.use(function(req, res, next) {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader('Access-Control-Allow-Methods', 'POST,GET,OPTIONS,PUT,DELETE');
@@ -24,13 +26,44 @@ app.use(cors({origin:true}));
   next();
 });*/
 
+// send email
+let transport = nodemailer.createTransport({
+  host: 'smtp.gmail.com',
+  port: 587,
+  auth: {
+    user: 'ammaressajee@gmail.com',
+    pass: 'zoo=2582924'
+  }
+});
+
+app.get('/sendmail', (req, res) => {
+  const message = {
+    from: 'user@example.com', // Sender address
+    to: 'ammaressajee@gmail.com', // List of recipients
+    subject: 'User Query', // Subject line
+    text: 'your message has been sent!' // Plain text body
+  };
+
+  transport.sendMail(message, function (err, info) {
+    if (err) {
+      console.log(err)
+    } else {
+      console.log(info);
+    }
+  });
+
+  res.render('mail', { page: 5, token: req.headers.cookie });
+
+});
+
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 //FOR UPLOADING+++
-var multer =require('multer');
+var multer = require('multer');
 //var upload=multer({ dest: '../images/' });
 //app.use(upload.array());
 //++++++++++++++++
@@ -44,18 +77,17 @@ app.use('/images', express.static('images'));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/news',newsRouter);
-app.use('/sports',sportsRouter);
-
+app.use('/news', newsRouter);
+app.use('/sports', sportsRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
