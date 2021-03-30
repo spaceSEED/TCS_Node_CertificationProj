b830bdbbab9011505963f3425b244e6567666410
ammaressajee <ammar.essajee97@gmail.com>
2021-03-30 11:27:14 -0500

    fixed contact page

--- frontend/src/app/components/gallery/gallery.component.ts
+++ frontend/src/app/components/gallery/gallery.component.ts
@@ -40,8 +40,8 @@ export class GalleryComponent implements OnInit {
   getImages(){
     this.serv.getImages().subscribe((res:any)=>{
       this.imgList[0]={active:true, title:res[0].title, img_url:res[0].img_url, description:res[0].description};
-      this.imgList[1]={active:true, title:res[1].title, img_url:res[1].img_url, description:res[1].description};
-      this.imgList[2]={active:true, title:res[2].title, img_url:res[2].img_url, description:res[2].description};
+      this.imgList[1]={active:false, title:res[1].title, img_url:res[1].img_url, description:res[1].description};
+      this.imgList[2]={active:false, title:res[2].title, img_url:res[2].img_url, description:res[2].description};
     });
   }
 }
