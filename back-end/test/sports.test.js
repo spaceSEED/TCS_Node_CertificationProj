aa4d22479c00ad1319738570c693d1ceb74f7c48
jiachengli1999 <35944405+jiachengli1999@users.noreply.github.com>
2021-03-31 11:04:00 -0400

    added sport test case

--- back-end/test/sports.test.js
+++ back-end/test/sports.test.js
@@ -6,59 +6,91 @@ const News = require('../src/models/news')
 const app = require('../src/app')
 const conn = require('../src/db/index')
 const request = require('supertest');
+const mongoose = require('mongoose')
+const User = require('../src/models/user')
 
 chai.use(chaiHttp);
 
 describe('Sports', () => {
+    let userDao = new User({
+        name: 'admin', 
+        email: 'admin@gmail.com',
+        password: '1234567'
+    })
+
+    let authenticatedUser = request.agent(app)
     // connect to db
     before((done) => {
         conn.connect()
-            .then(() => done())
-            .catch((err) => done(err));
+        .then(() => {
+            mongoose.connection.db.collection('news').deleteMany({})
+            mongoose.connection.db.collection('users').deleteMany({})
+            // create user
+            userDao.save()
+            .then(() => {
+                // login and get token
+                authenticatedUser.post('/users/login')
+                .send({email: 'admin@gmail.com', password: '1234567'})
+                .end((err, res) => {
+                    expect(res.statusCode).to.equal(302)
+                    expect('Location', '/')
+                    done()
+                })
+            })
+            .catch(err => console.log(err))
+        })
+        .catch((err) => console.log(err));
     })
 
     // disconnect from db
     after((done) => {
+        mongoose.connection.db.collection('news').deleteMany({})
+        mongoose.connection.db.collection('users').deleteMany({})
         conn.close()
-            .then(() => done())
-            .catch((err) => done(err));
+        .then(() => {
+            done()
+        })
+        .catch((err) => console.log(err));
+
     })
 
-    beforeEach((done) => {
-        News.remove({}, (err) => {
-            done();
-        });
-    });
+    it('should return status 200 for sports page', (done) => {
+        request(app).get('/sports')
+            .end((err, res) => {
+                expect(res.statusCode).to.equal(200)
+                done()
+            })
+    })
 
-    // it('should return status 200 for sports page', (done) => {
-    //     News.insertMany([
-    //         {
-    //             title: 'test1',
-    //             isSports: true
-    //         },
-    //         {
-    //             title: 'test2',
-    //             isSports: true
-    //         },
-    //         {
-    //             title: 'test3',
-    //             isSports: false
-    //         },
-    //     ])
-    //     .then(res =>{
-    //         request(app).get('/sports')
-    //         .then((res) => {
-    //             expect(res).to.have.status(200);
-    //             // console.log(res)
-    //             done();
-    //         })
-    //         .catch((err) => {
-    //             done(err);
-    //         });
-    //     })
-    //     .catch(err => done(err))
+    it('should return status 302 for posting sport news', (done) => {
+        authenticatedUser.post('/sports')
+            .send({
+                isSports: true,
+                img_url: 'pic2.jpg',
+                title: 'sportTest',
+                description: 'sportDescription',
+                pub_date: '2/2/2022',
+                url: 'https://www.google.com',
+                author: 'testAuthor'
+            })
+            .end((err, res) => {
+                expect(res.statusCode).to.equal(302)
+                done()
+            })
+    })
 
-    // });
+    it('should return status 400 for posting sport news with missing parameters', (done) => {
+        authenticatedUser.post('/sports')
+            .send({
+                isSports: true,
+                img_url: 'pic2.jpg',
+                author: 'testAuthor'
+            })
+            .end((err, res) => {
+                expect(res.statusCode).to.equal(400)
+                done()
+            })
+    })
 
 
    
