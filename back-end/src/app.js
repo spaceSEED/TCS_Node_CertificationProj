020453bf0b4b4e604937040f51a0e92f34690b42
Nicholas DeMarco <50844654+Ndemco@users.noreply.github.com>
2021-03-31 14:07:23 -0400

    refactor connection to db for tests

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -29,6 +29,7 @@ app.use(cors({ origin: true }));
 
 
 
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
---
+++ back-end/src/db/mongoose.test.js
@@ -0,0 +1,25 @@
+/* 
+    Provides a function to connect to the database for Mocha tests.
+    Since testing requires connecting / disconnecting from the database
+    multiple times, we wrap the parameterized mongoose.connect function
+    in another function to make the tests easier to write
+*/
+const mongoose = require('mongoose')
+
+async function connect() {
+    try {
+        await mongoose.connect(process.env.MONGODB_URL, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true
+        })
+    } catch (err) {
+        return { error: err }
+    }
+}
+
+function close() {
+    return mongoose.disconnect()
+}
+
+module.exports = { connect, close }
